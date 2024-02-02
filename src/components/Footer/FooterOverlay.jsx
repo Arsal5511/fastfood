4f4082e41c9ee282001eca390a15e4107fb5d1ea
Arslan <arslanabbas5511@gmail.com>
2024-02-02 23:00:31 +0500

    completed

--- src/components/Footer/FooterOverlay.jsx
+++ src/components/Footer/FooterOverlay.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-
 import './FooterOverlay.css';
 
-const FooterOverlay = () => (
-  <div>
-    FooterOverlay
+const FooterOverlay = () => {
+  
+  return(
+  <div className='app__footerOverlay'>
+    <div className='app__footerOverlay_black' />
+    <div className='app__footerOverlay_img app__bg'/>
   </div>
-);
+)};
 
 export default FooterOverlay;
--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import SubHeading from '../SubHeading/SubHeading';
 
 import './Newsletter.css';
 
-const Newsletter = () => (
-  <div>
-    Newsletter
+const Newsletter = () => {
+  return(
+  <div className='app_newsletter'>
+    <div className="newsletter_heading">
+      <SubHeading title='Newsletter' />
+      <h1 className='headtext__cormorant'>Subcribe to our Newsletter</h1>
+      <p className='p__opensans'>And Never Miss Latest Updates!</p>
+    </div>
+    <div className="newsletter_input flex__center">
+      <input type="email" placeholder="Enter your email address" />
+      <button type="button" className="custom__button">Subscribe</button>
+    </div>
   </div>
-);
+)};
 
 export default Newsletter;
